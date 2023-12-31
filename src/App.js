68d2502667879f47a6bd075519fd123c7c0e4384
Mahin Raza <mraza.saiyed@gmail.com>
2023-12-31 21:40:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
       <header className="App-header">
         <img src="/Logo.png" className="App-logo" alt="logo" />
         <p>
-          <code>Coming soon <br/>you can reach us on following email</code> 
+          <code>Coming soon <br/>you can reach us on below provided details</code> 
         </p>        
         <p>
-          <a style={{color:'white',textDecoration:'none'}} href="mailto:Theikoslab@gmail.com">Theikoslab@gmail.com</a> 
+          <a style={{color:'white',textDecoration:'none'}} href="mailto:Theikoslab@gmail.com">apple.poultry@gmail.com</a> 
         </p>
       </header>
     </div>
