a25b35f5119d21abebf3fdfecb06334388810480
Mahin Raza <mraza.saiyed@gmail.com>
2023-12-31 21:13:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/Logo.png" className="App-logo" alt="logo" />
+        <p>
+          <code>Coming soon <br/>you can reach us on following email</code> 
+        </p>        
+        <p>
+          <a style={{color:'white',textDecoration:'none'}} href="mailto:Theikoslab@gmail.com">Theikoslab@gmail.com</a> 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
